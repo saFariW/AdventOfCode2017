1114d64eb7f486ff6abe9df5ad620b83e9ad77a3
Wolters,Marc-Antoine M.A.P.L.M <marcantoine.wolters@student.fontys.nl>
2019-05-06 14:39:11 +0200

    ReWritten Day04 and added basic unitTests

--- AdventOfCode2017/AdventOfCode2017/Day04/SolutionDay04.cs
+++ AdventOfCode2017/AdventOfCode2017/Day04/SolutionDay04.cs
@@ -1,55 +1,33 @@
-﻿namespace AdventOfCode2017
+﻿namespace AdventOfCode2017.Day04
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
 
-    public static class SolutionDay04
+    public class SolutionDay04 : ISolution
     {
-        public static int Part01(List<List<string>> passPhrases)
+        public string GetName() => "Day 4: High-Entropy Passphrases";
+
+        public IEnumerable<object> Run(string input = null)
         {
-            return BasicSolution(passPhrases, Solution01Check);
+            input = input ?? Properties.Resources.InputDay04;
+            var formattedInput = input
+                .Split(Environment.NewLine)
+                .Select(x => x.Split(' ')
+                    .ToList())
+                .ToList();
+
+            // 2e param is the check on witch the passPhrase needs to comply. in this case no duplicates.
+            //  Thus querying the phrase for duplicates words, if there are give back false (does not comply).
+            yield return BaseSolution(formattedInput, list => !list.GroupBy(word => word).Any(g => g.Count() > 1));
+
+            // The little trick i perform here is ill sort the char's per word, and count how many same words there are in one line. If more than one it does not comply.
+            yield return BaseSolution(formattedInput, list => !list.Select(y => string.Join(string.Empty, y.ToCharArray().OrderBy(x => x))).GroupBy(word => word).Any(group => group.Count() > 1));
         }
 
-        public static int Part02(List<List<string>> passPhrases)
+        private static int BaseSolution(List<List<string>> passPhrases, Func<List<string>, bool> passPhraseCheck)
         {
-            return BasicSolution(passPhrases, Solution02Check);
-        }
-
-        public static int BasicSolution(List<List<string>> passPhrases, Func<List<List<string>>, int, int, int, bool> solutionCheck)
-        {
-            int totalCorrectPassPhrases = passPhrases.Count;
-
-            for (int x = 0; x < passPhrases.Count; x++)
-            {
-                bool passPhraseIsValid = true;
-                for (int i = 0; i < passPhrases[x].Count && passPhraseIsValid; i++)
-                {
-                    for (int j = 0; j < passPhrases[x].Count && passPhraseIsValid; j++)
-                    {
-                        if (j != i)
-                        {
-                            if (solutionCheck(passPhrases, i, j, x))
-                            {
-                                totalCorrectPassPhrases--;
-                                passPhraseIsValid = false;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return totalCorrectPassPhrases;
-        }
-
-        private static bool Solution01Check(List<List<string>> passPhrases, int i, int j, int x)
-        {
-            return passPhrases[x][i].Equals(passPhrases[x][j]);
-        }
-
-        private static bool Solution02Check(List<List<string>> passPhrases, int i, int j, int x)
-        {
-            return string.Concat(passPhrases[x][i].OrderBy(c => c)).Equals(string.Concat(passPhrases[x][j].OrderBy(c => c)));
+            return passPhrases.Where(passPhraseCheck).Count();
         }
     }
 }
---
+++ AdventOfCode2017/UnitTests/TestDay04.cs
@@ -0,0 +1,57 @@
+﻿namespace UnitTests
+{
+    using System;
+    using System.Linq;
+    using AdventOfCode2017.Day04;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestDay04
+    {
+        private SolutionDay04 sd04;
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.sd04 = new SolutionDay04();
+        }
+
+        [TestMethod]
+        public void TestInputRun01()
+        {
+            // single lines
+            Assert.AreEqual(1, this.sd04.Run("aa bb cc dd ee").ElementAt(0));
+            Assert.AreEqual(0, this.sd04.Run("aa bb cc dd aa").ElementAt(0));
+            Assert.AreEqual(1, this.sd04.Run("aa bb cc dd aaa").ElementAt(0));
+
+            // multi line input
+            Assert.AreEqual(2, this.sd04.Run("aa bb cc dd ee" + Environment.NewLine +
+                                             "aa bb cc dd aa" + Environment.NewLine +
+                                             "aa bb cc dd aaa").ElementAt(0));
+        }
+
+        [TestMethod]
+        public void TestInputRun02()
+        {
+            // single lines
+            Assert.AreEqual(1, this.sd04.Run("abcde fghij").ElementAt(1));
+            Assert.AreEqual(0, this.sd04.Run("abcde xyz ecdab").ElementAt(1));
+            Assert.AreEqual(1, this.sd04.Run("a ab abc abd abf abj").ElementAt(1));
+            Assert.AreEqual(1, this.sd04.Run("iiii oiii ooii oooi oooo").ElementAt(1));
+            Assert.AreEqual(0, this.sd04.Run("oiii ioii iioi iiio").ElementAt(1));
+
+            // multi line input
+            Assert.AreEqual(3, this.sd04.Run("abcde fghij" + Environment.NewLine +
+                                             "abcde xyz ecdab" + Environment.NewLine +
+                                             "a ab abc abd abf abj" + Environment.NewLine +
+                                             "iiii oiii ooii oooi oooo" + Environment.NewLine +
+                                             "oiii ioii iioi iiio").ElementAt(1));
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            this.sd04 = null;
+        }
+    }
+}
